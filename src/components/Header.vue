8a2dd0c53e5103b2d1243cbe8096fe8961168bce
chenuvi <chenuvi@outlook.com>
2020-12-15 22:18:19 +0800

    添加Header 组件

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <div>
+        <h2>header</h2>
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="css" scoped>
+    h2{
+        color:red
+    }
+</style>
