68e0ff6ca30a11999b160e54c291c32cc51b95ee
chenuvi <chenuvi@outlook.com>
2020-12-15 20:55:50 +0800

    Feat: mock 数据

--- build/dev-server.js
+++ build/dev-server.js
@@ -14,6 +14,37 @@ var port = process.env.PORT || config.dev.port
 var proxyTable = config.dev.proxyTable
 
 var app = express()
+
+const appData = require('../data.json')
+const seller = appData.seller
+const goods = appData.goods
+const ratings = appData.ratings
+
+const apiRoutes = express.Router()
+
+apiRoutes.get('/seller',function(req,res){
+  res.json({
+    errno:0,
+    data:seller
+  })
+})
+
+apiRoutes.get('/goods',function(req,res){
+  res.json({
+    errno:0,
+    data:goods
+  })
+})
+
+apiRoutes.get('/ratings',function(req,res){
+  res.json({
+    errno:0,
+    data:ratings
+  })
+})
+app.use('/api',apiRoutes)
+
+
 var compiler = webpack(webpackConfig)
 
 var devMiddleware = require('webpack-dev-middleware')(compiler, {
